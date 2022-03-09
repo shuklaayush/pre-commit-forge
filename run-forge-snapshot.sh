f563597d6566edfd685786002b46f7dcf9ddb59b
Ayush Shukla <shuklaayush247@gmail.com>
2022-03-09 22:24:44 +0530

    fix: match whole snapshot file name

--- run-forge-snapshot.sh
+++ run-forge-snapshot.sh
@@ -2,13 +2,11 @@
 
 SNAPSHOT_FILE=".gas-snapshot"
 
-git diff --cached --name-only | grep -qE '\.sol$'
-
 if [[ $? -ne 1 ]]; then
     echo "Running forge snapshot..."
     forge snapshot &> /dev/null
 
-    ( git ls-files --others --exclude-standard ; git diff --name-only ) | grep -qE "$SNAPSHOT_FILE"
+    ( git ls-files --others --exclude-standard ; git diff --name-only ) | grep -qw "$SNAPSHOT_FILE"
 
     if [[ $? -ne 1 ]]; then
         echo "Uncommited $SNAPSHOT_FILE file"
