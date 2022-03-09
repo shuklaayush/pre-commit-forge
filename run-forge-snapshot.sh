d36f837318efaf173aee8969ac08b11e4e20110a
Ayush Shukla <shuklaayush247@gmail.com>
2022-03-10 00:05:12 +0530

    fix: remove outdated if

--- run-forge-snapshot.sh
+++ run-forge-snapshot.sh
@@ -2,14 +2,12 @@
 
 SNAPSHOT_FILE=".gas-snapshot"
 
+echo "Running forge snapshot..."
+forge snapshot
+
+( git ls-files --others --exclude-standard ; git diff --name-only ) | grep -qw "$SNAPSHOT_FILE"
+
 if [[ $? -ne 1 ]]; then
-    echo "Running forge snapshot..."
-    forge snapshot
-
-    ( git ls-files --others --exclude-standard ; git diff --name-only ) | grep -qw "$SNAPSHOT_FILE"
-
-    if [[ $? -ne 1 ]]; then
-        echo "Uncommited $SNAPSHOT_FILE file"
-        exit 1
-    fi
+    echo "Uncommited $SNAPSHOT_FILE file"
+    exit 1
 fi
