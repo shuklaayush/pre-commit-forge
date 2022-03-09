dc251f6870e181334307cfa24ed97403b1653f9e
Ayush Shukla <shuklaayush247@gmail.com>
2022-03-10 02:00:02 +0530

    feat: run only on staged changes

--- run-forge-snapshot.sh
+++ run-forge-snapshot.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+GIT_STASH_MSG="run-forge-snapshot-stash"
+
 SNAPSHOT_FILE=".gas-snapshot"
 ARGS="$@"
 
@@ -11,7 +13,19 @@ while (( "$#" )); do
   shift
 done
 
+git ls-files --others --exclude-standard | grep -qw "$SNAPSHOT_FILE"
+if [[ $? -ne 1 ]]; then
+  rm "$SNAPSHOT_FILE"
+fi
+
+git diff --name-only | grep -qw "$SNAPSHOT_FILE"
+if [[ $? -ne 1 ]]; then
+  git checkout -- "$SNAPSHOT_FILE"
+fi
+
+git stash --keep-index --include-untracked -m "${GIT_STASH_MSG}"
 forge snapshot $ARGS
+git stash list | grep -qw "${GIT_STASH_MSG}" && git stash pop -q
 
 ( git ls-files --others --exclude-standard ; git diff --name-only ) | grep -qw "$SNAPSHOT_FILE"
 
